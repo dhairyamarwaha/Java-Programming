2ac001a642086f5861fb15adce3d11620ba5d38d
Dhairya Marwaha <dhairyamarwaha@Dhairyas-MacBook-Pro.local>
2025-06-19 22:36:05 +0530

    testing

--- src/main/java/FibonnaciSeries/FibonnaciSeries.java
+++ src/main/java/FibonnaciSeries/FibonnaciSeries.java
@@ -1,4 +1,25 @@
 package FibonnaciSeries;
 
+import java.util.Scanner;
+
 public class FibonnaciSeries {
+    Scanner sc = new Scanner(System.in);
+    int terms;
+    int firstTerm = 0;
+    int secondTerm = 1;
+
+    public void getTerms(){
+        System.out.print("Please enter the number of terms you wish to go till : ");
+        terms = sc.nextInt();
+    }
+
+    public void printSeries(){
+        for (int index = 1; index <= terms; index++){
+            System.out.print(firstTerm + ", ");
+
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+    }
 }
